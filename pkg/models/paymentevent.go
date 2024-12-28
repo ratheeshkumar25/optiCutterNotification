67ec9e20163e8cb1693cd1e15001316d1d5f9bbe
ratheeshkumar25 <ratheeshgopinadhkumar@gmail.com>
2024-12-28 13:00:55 +0530

    Updated content in notification-service

--- pkg/models/paymentevent.go
+++ pkg/models/paymentevent.go
@@ -8,12 +8,3 @@ type PaymentEvent struct {
 	Amount    float64 `json:"amount"`
 	Date      string  `json:"date"`
 }
-
-// type PaymentEvent struct {
-// 	PaymentID string  `bson:"payment_id"`
-// 	OrderID   uint    `bson:"order_id"`
-// 	UserID    uint    `bson:"user_id"`
-// 	Email     string  `bson:"email"`
-// 	Amount    float64 `bson:"amount"`
-// 	Date      string  `bson:"date"`
-// }
